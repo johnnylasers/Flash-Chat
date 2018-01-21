8f1a02a16ffdff66ecc6ae5b1d6b34838ad4b0b3
johnnylasers <jianchen85@gmail.com>
2018-01-21 10:34:45 -0800

    registration/login/logout flow completed

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -109,6 +109,7 @@ class ChatViewController: UIViewController {
         //TODO: Log out the user and send them back to WelcomeViewController
         do {
             try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
 
         } catch {
             print("ERROR! signing out failed.....")
